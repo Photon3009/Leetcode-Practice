8a6cb6f8e6fe8b43503095300739d47323cb5979
Shivam Verma <100941430+Photon3009@users.noreply.github.com>
2024-07-16 20:08:24 +0530

    Time: 102 ms (7.17%), Space: 96.2 MB (7.21%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int maxProfit=0;
-        int mini=prices[0];
-        for(int i=1;i<n;i++){
-            int cost=prices[i]-mini;
-            maxProfit=max(cost,maxProfit);
-            mini=min(mini,prices[i]);
+         if (prices.empty()) return 0;
+        
+        int buy = 0; // Buy pointer
+        int max_profit = 0;
+
+        for (int sell = 1; sell < prices.size(); ++sell) {
+            if (prices[sell] < prices[buy]) {
+                buy = sell; // Update buy pointer if current price is lower
+            } else {
+                int profit = prices[sell] - prices[buy];
+                max_profit = std::max(max_profit, profit);
+            }
         }
-        return maxProfit;
+
+        return max_profit;
+        // Mine approach below
+        // int n=prices.size();
+        // int maxProfit=0;
+        // int mini=prices[0];
+        // for(int i=1;i<n;i++){
+        //     int cost=prices[i]-mini;
+        //     maxProfit=max(cost,maxProfit);
+        //     mini=min(mini,prices[i]);
+        // }
+        // return maxProfit;
     }
 };
