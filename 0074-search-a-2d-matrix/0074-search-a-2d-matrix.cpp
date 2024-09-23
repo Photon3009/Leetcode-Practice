7510a7c8cda54532e2ab8bffc266b4b60582cd82
Shivam Verma <100941430+Photon3009@users.noreply.github.com>
2024-09-23 15:56:30 +0530

    Time: 0 ms (100%), Space: 12.2 MB (79.44%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int beg=0;
-        int end=m-1;
-        while(beg<=end){
-            int mid=(beg+end)/2;
-            if(matrix[mid][0]<=target&&target<=matrix[mid][n-1]){
-                int beg1=0;
-                int end1=n-1;
-                while(beg1<=end1){
-                    int mid1=(beg1+end1)/2;
-                    if(target==matrix[mid][mid1]) return true;
-                      if(target>matrix[mid][mid1]){
-                        beg1=mid1+1;
-                     }else{
-                        end1=mid1-1;
-                     }
-                }
-                return false;
-            }
-            if(target<matrix[mid][0]){
-                end=mid-1;
-            }
-            else{
-                beg=mid+1;
-            }
+        
+         if (matrix.empty() || matrix[0].empty()) return false;
+
+    int m = matrix.size();
+    int n = matrix[0].size();
+    int left = 0, right = m * n - 1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        int row = mid / n;
+        int col = mid % n;
+        int midElement = matrix[row][col];
+        
+        if (midElement == target) {
+            return true;  // Target found
+        } else if (midElement < target) {
+            left = mid + 1;  // Search right half
+        } else {
+            right = mid - 1;  // Search left half
         }
-        return false;
+    }
+    
+    return false; 
+
+
     }
 };
