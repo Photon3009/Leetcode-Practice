238f204c7f7f53c57c985a9f79c3afc0e305898d
Shivam Verma <100941430+Photon3009@users.noreply.github.com>
2024-10-03 13:05:16 +0530

    Time: 3 ms (64.48%), Space: 11.6 MB (12.01%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,34 +1,50 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n=nums.size();
-        int s=1;
-        int e=n-2;
-        int mid;
-        if(n==1){
-            return 0;
-        }
-        if(nums[0]>nums[1]){
-            return 0;
-        }
-        if(nums[n-1]>nums[n-2]){
-            return n-1;
-        }
-        while(s<=e){
-            mid=(s+e)/2;
+        // int n=nums.size();
+        // int s=1;
+        // int e=n-2;
+        // int mid;
+        // if(n==1){
+        //     return 0;
+        // }
+        // if(nums[0]>nums[1]){
+        //     return 0;
+        // }
+        // if(nums[n-1]>nums[n-2]){
+        //     return n-1;
+        // }
+        // while(s<=e){
+        //     mid=(s+e)/2;
             
-            if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]){
-                return mid;
-            }
-            if(nums[mid]>nums[mid-1]){
-                s=mid+1;
-            }
-            else{
-                e=mid-1;
-            }
-            
-            
+        //     if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]){
+        //         return mid;
+        //     }
+        //     if(nums[mid]>nums[mid-1]){
+        //         s=mid+1;
+        //     }
+        //     else{
+        //         e=mid-1;
+        //     }
+        // }
+        // return mid;
+        int left = 0;
+    int right = nums.size() - 1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2; // To avoid potential overflow
+        
+        // Compare mid element with its right neighbor
+        if (nums[mid] > nums[mid + 1]) {
+            // There is a peak on the left side (including mid)
+            right = mid; // Search in the left half
+        } else {
+            // There is a peak on the right side
+            left = mid + 1; // Search in the right half
         }
-        return mid;
+    }
+    
+    // left will point to a peak element
+    return left;
     }
 };
